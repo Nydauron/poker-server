831871d934c9f2546c3e73172030b371b3c9884f
Jareth Gomes <jarethgomes@gmail.com>
2022-05-19 21:07:22 -0500

    Fixed table struct errors

--- src/poker/mod.rs
+++ src/poker/mod.rs
@@ -1,6 +1,8 @@
 pub mod pokerdeck;
 pub mod games;
 pub mod player;
+pub mod table;
 pub use self::pokerdeck::*;
 pub use self::games::*;
 pub use self::player::*;
+pub use self::table::*;
--- src/poker/table.rs
+++ src/poker/table.rs
@@ -1,12 +1,12 @@
-use crate::Player;
-
-mod player;
+use crate::poker::Player;
+use crate::poker::GameVariation;
 
 pub struct Table {
     players: Vec<Player>,
-    game: GameVariation,
     
     big_blind_idx: usize,
     btn_idx: usize,
     pots: Vec<u64>, // might make as a struct that handles side pots
+
+    game: dyn GameVariation,
 }
