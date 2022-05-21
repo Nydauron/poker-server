0cdfb671082a8a32c394d3085faef6460d3152f3
Jareth Gomes <jarethgomes@gmail.com>
2022-05-21 02:43:53 -0500

    Added implementation for adding bet to no limit pot

--- src/poker/pot.rs
+++ src/poker/pot.rs
@@ -5,9 +5,9 @@ pub struct Pot<'a> {
     elegible_players: Vec<Vec<&'a Player>>,
 
     largest_bet_idx: usize, // should be set when a bet larger than largest_bet is made
-    largest_bet: usize,     // should be set when a bet larger than largest_bet is made
+    largest_bet: u64,       // should be set when a bet larger than largest_bet is made
 
-
+    bet_diff: u64,          // used to determine the minimum amount to raise by
 }
 
 impl<'a> Pot<'a> {
@@ -17,10 +17,23 @@ impl<'a> Pot<'a> {
             elegible_players: Vec::new(),
             largest_bet_idx: 0,
             largest_bet: 0,
+            bet_diff: 0,
         }
     }
 
     pub fn are_all_bets_good(& self, action_idx: usize) -> bool {
         self.largest_bet_idx == action_idx
+    }
+
+    // This is the function that will work for No Limit
+    // TODO: Need to polymorphisize this. Turn struct Pot into a trait that is used in NoLimitPot, LimitPot, and PotLimitPot.
+    pub fn set_highest_bet(&mut self, action_idx: usize, new_bet: u64) -> Result<(), &str> {
+        if self.bet_diff + self.largest_bet > new_bet {
+            return Err("Bet not high enough");
+        }
+        self.bet_diff = new_bet - self.largest_bet;
+        self.largest_bet = new_bet;
+        self.largest_bet_idx = action_idx;
+        Ok(())
     }
 }
